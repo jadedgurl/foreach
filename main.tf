3dc89a523396b7d84e0ee7cd514abe2451873054
jadedgurl <126923594+jadedgurl@users.noreply.github.com>
2023-06-05 20:30:35 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,14 +9,12 @@ provider "aws" {
 data "aws_availability_zones" "all" {}
 
   # Use for_each to loop over var.custom_tags
-  dynamic "tag" {
     for_each = var.custom_tags
      tags = {
     Name        = "My bucket"
     Environment = "Dev"
     }
   }
-}
 
 resource "aws_s3_bucket" "example" {
   bucket = "my-tf-test-bucket"
